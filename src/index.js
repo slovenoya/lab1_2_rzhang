eb0729601cba782ac348b83a866c39685a655b98
ryan zhang <rzhang21@calpoly.edu>
2021-09-26 21:54:06 -0700

    first git commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import MyApp from './MyApp'
+
+ReactDOM.render(<MyApp />, document.getElementById('root'))
