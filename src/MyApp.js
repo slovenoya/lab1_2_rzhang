af8b5b069c030c94edcb3d748eec3fc3f109bf55
slovenoya <rzhang21@calpoly.edu>
2021-10-03 14:42:37 -0700

    try on 4-2

--- src/MyApp.js
+++ src/MyApp.js
@@ -4,60 +4,59 @@ import Form from './Form'
 import axios from 'axios';
 
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
+   const [characters, setCharacters] = useState([]);
       
-    function removeOneCharacter (index) {
-        const updated = characters.filter((character, i) => {
+   function removeOneCharacter (index) {
+         const updated = characters.filter((character, i) => {
             return i !== index
-        });
-        setCharacters(updated);
-    }
-    
-      useEffect(() => {
-        fetchAll().then( result => {
-           if (result)
-              setCharacters(result);
          });
-     }, [] );
-
-     function updateList(person) { 
-      makePostCall(person).then( result => {
-      if (result)
-         setCharacters([...characters, person] );
+         setCharacters(updated);
+   }
+    
+   useEffect(() => {
+      fetchAll().then( result => {
+         if (result)
+            setCharacters(result);
       });
-    }
+   }, [] );
 
-    return (
-        <div className="container">
-          <Table characterData={characters} removeCharacter={removeOneCharacter} />
-          <Form handleSubmit={updateList} />
-        </div>
-      )
+   function updateList(person) { 
+      makePostCall(person).then( result => {
+         if (result)
+            setCharacters([...characters, person] );
+         });
+   }
+
+   return (
+      <div className="container">
+         <Table characterData={characters} removeCharacter={removeOneCharacter} />
+         <Form handleSubmit={updateList} />
+      </div>
+   )
 }
 
 async function fetchAll(){
-  try {
-     const response = await axios.get('http://localhost:5000/users');
-     return response.data.users_list;     
-  }
-  catch (error){
-     //We're not handling errors. Just logging into the console.
-     console.log(error); 
-     return false;         
-  }
+   try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;     
+   }
+   catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+   }
 }
 
 async function makePostCall(person){
-  try {
-     const response = await axios.post('http://localhost:5000/users', person);
-     return response.data;
-  }
-  catch (error) {
-     console.log(error);
-     return false;
-  }
+   try {
+      const response = await axios.post('http://localhost:5000/users', person);
+      console.log(response.data)
+      return response.data;
+   }
+   catch (error) {
+      console.log(error);
+      return false;
+   }
 }
-
-
 
 export default MyApp;
